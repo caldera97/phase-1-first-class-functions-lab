9f4cd24038882381de22eb622ac20a39fb70ae55
David Caldera <davidcaldera13@yahoo.com>
2022-02-23 17:18:38 -0600

    commit tree 70f72de317aacb0c7b9d9c7ccfdcf0a66b93e09d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers= (array) => {
+    let sliced = array.slice(0,2)
+     return sliced;
+    }
+
+const returnLastTwoDrivers= (array) => {
+        let sliced = array.slice(-2)
+         return sliced;
+        }
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num1) {
+    let mult = function(price) {
+        return price * num1
+    }
+    return mult;
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
+}
